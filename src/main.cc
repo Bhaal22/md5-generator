0daa9c6c11605bbcf9d62c9e5276b49b01427fe4
Bhaal22 <muller.john@gmail.com>
2014-08-31 11:33:45 +0200

    used of boost::program_option to parse command line parameters

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,22 @@
-//#include <boost/program_options/option.hpp>
+#include <boost/program_options/options_description.hpp>
 
 #include <iostream>
 #include "FileSystemCrawler.h"
 
+namespace po = boost::program_options;
+
 int main(int argc, char **argv)
 {
+  po::options_description description("Hash Generator usage");
+
+
+
+
 	FileSystemCrawler crawler;
 
 
 	std::list<std::string> dirs;
 
-  std::cout << "coucou" << std::endl;
 	dirs.push_front("D:\\dev\\monitor\\10.9.dev\\TEST_MONITOR\\Bin");
 
 	crawler.crawl(dirs);
